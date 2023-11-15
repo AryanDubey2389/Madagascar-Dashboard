5dd7c49b4b247fa23f24f009faeac9641f8e90b2
AryanDubey2389 <aryan.dubey@research.iiit.ac.in>
2023-11-15 14:05:50 +0530

    Complete Project

--- EVST Dashboard/src/views/Map.js
+++ EVST Dashboard/src/views/Map.js
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import map from '../assets/img/madagascar_map.png';
 import point from '../assets/img/imp_points.png';
+import point2 from '../assets/img/point2.png';
+import map2 from '../assets/img/map2.png'
 
-function Map() {
+function MapSelector() {
   const [isZoomed, setIsZoomed] = useState(false);
+  const [selectedOption, setSelectedOption] = useState("map");
 
   const handleMouseEnter = () => {
     setIsZoomed(true);
@@ -13,10 +16,14 @@ function Map() {
     setIsZoomed(false);
   };
 
-  return (
-    <>
-      <div className="content" style={{ overflow: 'hidden', position: 'relative' }}>
-      <img
+  const handleDropdownChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
+  const Map1 = () => {
+    return (
+      <>
+            <img
               src={point}
               alt="Point"
               style={{
@@ -52,9 +59,73 @@ function Map() {
             }}
           />
         </div>
+      </>
+    );
+  };
+  const Map2 = () => {
+    return (
+      <>
+            <img
+              src={point2}
+              alt="Point"
+              style={{
+                position: 'relative',
+                top: '50%', // Adjust as needed
+                left: '30%', // Adjust as needed
+                width: '420px', // Adjust as needed
+                height: '120px', // Adjust as needed
+                transform: 'translate(-50%, -50%)', // Center the point
+                zIndex: 2, // Ensure the point is above the map
+              }}
+            />
+        <div
+          style={{
+            overflow: 'auto',
+            width: isZoomed ? '100%' : '350px', // Initial width is 350px
+            height: 'auto',
+            position: 'relative',
+            margin: '0 auto',
+            transition: 'width 0.5s ease-in-out',
+          }}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          <img
+            src={map2}
+            alt="Map"
+            style={{
+              width: '100%', // Use 100% width inside the container
+              height: 'auto',
+              display: 'block',
+              margin: '0 auto',
+            }}
+          />
+        </div>
+      </>
+    );
+  };
+  const renderSelectedComponent = () => {
+    switch (selectedOption) {
+      case "map1":
+        return <Map1 />;
+      case "map2":
+        return <Map2 />;
+      default:
+        return <Map1 />;
+    }
+  };
+
+  return (
+    <>
+      <div className="content" style={{ overflow: 'hidden', position: 'relative' }}>
+        <select onChange={handleDropdownChange}>
+          <option value="map1">Tree Cover</option>
+          <option value="map2">Forest Change</option>
+        </select>
+        {renderSelectedComponent()}
       </div>
     </>
   );
 }
 
-export default Map;
+export default MapSelector;
--- EVST Dashboard/src/views/TableList.js
+++ EVST Dashboard/src/views/TableList.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 // reactstrap components
 import {
   Card,
@@ -109,7 +109,60 @@ function Tables() {
                 <CardTitle tag="h4">Analysis of above data</CardTitle>
               </CardHeader>
               <CardBody>
-                Hi there I am here!!
+              <div className="typography-line">
+                  <span>Cover Tree Loss Trend Analysis</span>
+                  <blockquote>
+                    <p className="blockquote blockquote-primary">
+                    Initial Stability and Subsequent Increase: Madagascar's biodiversity is incredibly diverse but also highly endangered. The initial stability might reflect a period of equilibrium or insufficient data collection. The increase in biodiversity metrics could be due to improved conservation efforts, international aid, or environmental policies that were effective during that period.
+Sharp Decline After 2016: The decline could be attributed to several factors such as increased deforestation, political instability affecting conservation efforts, the impact of climate change, and a rise in illegal wildlife trade. Additionally, specific events such as severe weather, droughts, or cyclones, which are not uncommon in the region, could have contributed to a sudden loss in biodiversity.
+Stabilization Post-2019: This may indicate that some of the detrimental effects have been curbed or that mitigation efforts have begun to show results. Alternatively, it might also reflect a new baseline where the biodiversity has stabilized at a lower level due to permanent losses.
+                    </p>
+                  </blockquote>
+                </div>
+                <div className="typography-line">
+                <span>Weekly Fire Alerts Trend Analysis</span>
+                  <blockquote>
+                    <p className="blockquote blockquote-primary">
+                    Low Activity in Early Weeks: This could correspond to the wet season in Madagascar, which typically experiences fewer fires.
+Increase Around Week 17 Onwards: The increase aligns with the dry season, which usually starts in May and lasts until October. During this period, the risk of fires can be higher due to dry vegetation, and slash-and-burn agriculture (tavy) can contribute significantly to these numbers.
+Peak and Fluctuations: The peak around mid-year could coincide with the height of the dry season, where conditions for fires are most prevalent. Fluctuations might result from variations in human activity, enforcement of fire management policies, or natural factors like sporadic rain.
+Decrease Towards Year-End: This decline likely corresponds to the onset of the wet season, where rainfall reduces the likelihood of fires.
+For Madagascar specifically, deforestation is a significant concern, often driven by slash-and-burn agriculture, logging for timber, and the production of charcoal. These activities can contribute to both the loss of biodiversity and increase in fire alerts.
+                    </p>
+                  </blockquote>
+                </div>
+                <div className="typography-line">
+                <span>Yearly Fire Alerts Trend Analysis</span>
+                  <blockquote>
+                    <p className="blockquote blockquote-primary">
+                    Improved Fire Management and Prevention Policies:  Madagascar has implemented more effective fire management and prevention policies, including increased awareness, surveillance, and stricter regulations, it could contribute to a decline in forest fires.
+
+Community Involvement and Education: Engaging local communities in forest protection and providing education on fire prevention methods can be crucial. If there have been successful community-based initiatives to prevent and manage fires, it could lead to a reduction in incidents.
+
+Climate and Weather Patterns: Natural factors, such as changes in climate and weather patterns, can influence the occurrence and intensity of forest fires. If there have been shifts in weather conditions that are less conducive to fires, it could contribute to a decreasing trend.
+
+Satellite Monitoring and Early Warning Systems: Advances in technology, including satellite monitoring and early warning systems, can help detect and respond to fires more rapidly. Timely intervention can prevent fires from spreading and causing extensive damage logging for timber, and the production of charcoal. These activities can contribute to both the loss of biodiversity and increase in fire alerts.
+                    </p>
+                  </blockquote>
+                </div>
+                <div className="typography-line">
+                <span>Greenhouse Emission Trend Analysis</span>
+                  <blockquote>
+                    <p className="blockquote blockquote-primary">
+                    The greenhouse emission trend for Madagascar is complex and influenced by a variety of factors, including economic growth, population growth, climate change, and government policies. While the overall trend has been relatively stable in recent years, there have been periods of both increasing and decreasing emissions.
+
+From 2001 to 2007, Madagascar's greenhouse gas emissions rose steadily, driven by rapid economic growth and population expansion. The average annual economic growth rate during this period was 5.6%, while the population grew at an average rate of 2.6%. This growth led to increased energy consumption and associated emissions.
+
+Between 2008 and 2010, Madagascar experienced a decline in greenhouse gas emissions, coinciding with the global financial crisis. The economic downturn resulted in slower economic growth, with an average annual growth rate of just 0.3%. This slowdown in economic activity contributed to a reduction in emissions.
+
+Since 2011, Madagascar's greenhouse gas emissions have remained relatively stable, fluctuating within a narrow range. This stability can be attributed to a combination of factors, including continued economic growth, population growth, climate change, and government policies. The economy has grown at an average annual rate of 4.0% since 2011, while the population has continued to expand at an average rate of 2.5%. Climate change is also likely playing a role in emissions levels, although its precise impact is difficult to quantify. The government of Madagascar has implemented a number of policies aimed at promoting renewable energy and energy efficiency, which may be helping to mitigate emissions.
+
+In 2019, Madagascar's greenhouse gas emissions increased by 1.6%, suggesting a potential shift away from the recent stability. This increase could be due to various factors, such as changes in energy consumption patterns, agricultural practices, or land use.
+
+Overall, Madagascar's greenhouse emission trend reflects the interplay of economic, demographic, environmental, and policy factors. While the country has made progress in stabilizing emissions in recent years, the future trajectory will depend on how these factors evolve. Continued economic growth and population expansion could put upward pressure on emissions, while climate change impacts and government policies could have either mitigating or exacerbating effects. Understanding the complex dynamics of Madagascar's emission trends is crucial for developing effective strategies to reduce greenhouse gas emissions and promote sustainable development in the country.
+                    </p>
+                  </blockquote>
+                </div>
               </CardBody>
             </Card>
           </Col>
